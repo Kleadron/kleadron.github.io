e73f2a979fd961c2901195473bad7b37d5b1dafc
Kleadron <kleadron04@gmail.com>
2021-01-18 19:59:38 -0800

    add the thingy

--- resources/scripts/globals.js
+++ resources/scripts/globals.js
@@ -1,6 +1,6 @@
 //global website name
 site_name = 'Kleadron&#39;s Site';
-site_copyright = 'Copyright Aidan Hewitt 2019-2021';
+site_copyright = 'Copyright &#169; Aidan Hewitt 2019-2021';
 
 //news in header
 news_message = 'New Minecraft 1.2.5 mod!';
