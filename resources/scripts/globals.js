78c103b13420418960849e24c8a3c41b62ce9b7e
Kleadron <kleadron04@gmail.com>
2021-04-30 14:09:50 -0700

    Website update april 2021

--- resources/scripts/globals.js
+++ resources/scripts/globals.js
@@ -3,8 +3,8 @@ site_name = 'Kleadron&#39;s Site';
 site_copyright = 'Copyright &#169; Aidan Hewitt 2019-2021. All rights reserved.';
 
 //news in header
-news_message = 'New Minecraft 1.2.5 mod!';
-news_link = 'mod_preloader.html';
+news_message = 'FortressCraft Chapter 1 is out!';
+news_link = 'https://store.steampowered.com/app/1558210/FortressCraft__Chapter_1/';
 
 //alert box
 alert_message = '';
