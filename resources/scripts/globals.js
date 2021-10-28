721fc051c656451ab5d985db98ff5af2ba914d6b
Kleadron <kleadron04@gmail.com>
2021-10-27 19:06:37 -0700

    Music demos page

--- resources/scripts/globals.js
+++ resources/scripts/globals.js
@@ -1,10 +1,10 @@
 //global website name
 site_name = 'Kleadron&#39;s Site';
-site_copyright = 'Copyright &#169; Aidan Hewitt 2019-2021. All rights reserved.';
+site_copyright = 'Copyright &#169; 2019-2021';
 
 //news in header
-news_message = 'FortressCraft Chapter 1 is out!';
-news_link = 'https://store.steampowered.com/app/1558210/FortressCraft__Chapter_1/';
+news_message = '';
+news_link = '';
 
 //alert box
 alert_message = '';
