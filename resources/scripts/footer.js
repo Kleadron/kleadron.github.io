1e4e4b58a9813196d3aa133fb0707f69b8d71850
Kleadron <kleadron04@gmail.com>
2025-05-17 01:47:26 -0700

    Update footer.js

--- resources/scripts/footer.js
+++ resources/scripts/footer.js
@@ -4,10 +4,10 @@ document.write('\
     	<table class="table-generic" width="100%" cellpadding="0" cellspacing="0">\
     		<tr>\
     			<td align="left">\
-    				<p style="text-align:center;">' + site_copyright + '</p>\
+    				<p style="text-align:left;"> ' + site_copyright + '</p>\
     			</td>\
     			<td align="right">\
-    				<a href="https://validator.w3.org/markup/check?uri=referer"><img src="https://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Transitional" height="31" width="88"></a>\
+    				<a href="https://validator.w3.org/markup/check?uri=referer"><img src="https://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Transitional" height="31" width="88"> </a>\
     			</td>\
     		</tr>\
     	</table>\
