5dbff8fc1926f266c493f3971cef5f7561b48b79
Kleadron <kleadron04@gmail.com>
2025-05-17 19:50:30 -0700

    latest style changes

--- resources/scripts/footer.js
+++ resources/scripts/footer.js
@@ -7,7 +7,7 @@ document.write('\
     				<em><p>' + site_copyright + '</p></em>\
     			</td>\
     			<td align="right">\
-    				<a target="_blank" href="https://validator.w3.org/check?uri=https%3A%2F%2Fkleadron.net%2F"><img src="https://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Transitional" height="31" width="88"></a>\
+    				<a target="_blank" href="https://validator.w3.org/check?uri=https%3A%2F%2Fkleadron.net%2F"><img src="resources/images/valid-html401.png" alt="Valid HTML 4.01 Transitional" height="31" width="88"></a>\
     			</td>\
     		</tr>\
     	</table>\
